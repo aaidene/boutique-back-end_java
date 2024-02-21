4e4ce9414013ab919551f82721dfab186e8239a7
aaidene <aidene.abdelhakim@hotmail.fr>
2024-02-21 01:11:39 +0100

    Premier version de back-end java de projet boutique

---
+++ src/main/java/com/greta/LaBoutique/LaBoutiqueApplication.java
@@ -0,0 +1,12 @@
+package com.greta.LaBoutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.greta.LaBoutique")
+public class LaBoutiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaBoutiqueApplication.class, args);
+    }
+}
